ee6cc9a41d0ddfff67c7a1f944e1d04afbd1172f
IrvinEscalante <shadow_punk16@hotmail.com>
2017-10-23 20:07:15 -0500

    Initial commit

---
+++ resources/views/inventario/login.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+	<link href="css/estilos.css" rel="stylesheet" type="text/css">
+</head>
+	<body>
+	<!--header -->
+	<div class="header1">
+		<img class="heraderimage" src="imagenes/imss.PNG">		
+	</div>
+	<div class="body">
+		<div class="login">
+			<form action="" method="post">
+				User name:
+				<input type="text" name="userid">
+				<br><br>
+				User password:
+				<input type="password" name="psw">
+				<br><br>
+				<input type="submit" value="Ingresar"></input>
+			</form>
+		</div>
+	</div>
+	
+	<!--banner -->
+	<!--content -->
+	<!--footer -->
+	</body>
+</html>
